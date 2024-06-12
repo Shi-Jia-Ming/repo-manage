c190d7fb329ad7b84ac1faa8b5d24abbc8798985
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-06-12 22:36:19 +0800

    feat($ui): remove demo code
    remove component `HelloWorld` and replace it with span `Hello World!`

--- components.d.ts
+++ components.d.ts
@@ -7,8 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
