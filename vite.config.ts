e328b19fe91528ce5315d5b6a913efa5b6d386ab
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-06-12 23:04:43 +0800

    chore($ui): add `element-plus` package
    add `element-plus` package and config auto import.

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,45 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect'
+import {resolve} from 'path';
+
+const pathSrc = resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': pathSrc,
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+      dts: resolve(pathSrc, 'auto-imports.d.ts'),
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    })
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+      ],
+      dts: resolve(pathSrc, 'components.d.ts'),
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+    Inspect(),
   ],
   css: {
     preprocessorOptions: {
