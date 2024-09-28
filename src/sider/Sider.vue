45890383bc094f727109a54f8b13cab33fa40374
Shi-Jia-Ming <2516844846@qq.com>
2024-09-28 18:44:41 +0800

    perf(*): remove unused sider

--- src/sider/Sider.vue
+++ src/sider/Sider.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Repository from "@/sider/components/Repository.vue";
-import HelloWorld from "@/sider/components/HelloWorld.vue";
 import Document from "@/sider/components/Document.vue";
 
 const props = defineProps<{
@@ -10,8 +8,6 @@ const props = defineProps<{
 
 <template>
   <div class="sider-layout">
-    <repository v-if="props.currentComponent === 'repository'"/>
-    <hello-world v-if="props.currentComponent === 'hello-world'"/>
     <document v-if="props.currentComponent === 'document'"/>
   </div>
 </template>
